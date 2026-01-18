f38286d200a5a4a68efad10e48a3ee2e118130f6
Saurabh Daga <saurabhdaga@ymail.com>
2026-01-19 00:32:23 +0530

    remove board led api calls. they are replaced by gpio module

--- platform/common/include/board_api.h
+++ platform/common/include/board_api.h
@@ -2,17 +2,6 @@
 
 void board_init(void);
 
-#ifdef BOARD_HAS_LED
-/* LEDs */
-void board_led_on(void);
-void board_led_off(void);
-void board_led_toggle(void);
-#endif
-
-#ifdef BOARD_HAS_BUTTON
-bool board_button_is_pressed(void);
-#endif
-
 #ifdef BOARD_HAS_CONSOLE
 void board_console_init(void);
 #endif
