d29065591eca252504af94906e8c2ec8c341d6f4
Saurabh Daga <saurabhdaga@ymail.com>
2026-01-19 00:37:52 +0530

    update led blinking project. it now uses gpio api calls to interact with LED GPIO as well as uses updated time api calls

--- app/projects/blinky/main.c
+++ app/projects/blinky/main.c
@@ -1,12 +1,17 @@
+#include "gpio.h"
+#include "pinmap.h"
 #include "board_api.h"
+#include <unistd.h>
 
 int main(void)
 {
     board_init();
 
+    gpio_init(LED_PIN, GPIO_MODE_OUTPUT, GPIO_PULL_NONE);
+
     while (1) {
-        board_led_toggle();
-        for (volatile int i = 0; i < 200000; i++);
+        gpio_toggle(LED_PIN);
+        sleep(1);
     }
 }
 
