eec99823d91ebcc2f0c0b74a773929df446af015
Saurabh Daga <saurabh.daga@semaconnect.com>
2026-02-27 10:52:31 +0530

    Ad guard conditions to prevent from potential segfaults.

--- platform/common/src/gpio/src/gpio.c
+++ platform/common/src/gpio/src/gpio.c
@@ -17,24 +17,28 @@ void gpio_register(const struct gpio_ops * ops)
 
 void gpio_init(gpio_pin_t pin, gpio_mode_t mode, gpio_pull_t pp)
 {
-    if(g_gpio.ops->init) g_gpio.ops->init(pin, mode, pp);
+	if(!g_gpio.initialized) return;
+    if(g_gpio.ops && g_gpio.ops->init) g_gpio.ops->init(pin, mode, pp);
 }
 
 void gpio_write(gpio_pin_t pin, gpio_level_t level)
 {
-    if(g_gpio.ops->write) g_gpio.ops->write(pin, level);
+	if(!g_gpio.initialized) return;
+    if(g_gpio.ops && g_gpio.ops->write) g_gpio.ops->write(pin, level);
 }
 
 gpio_level_t gpio_read(gpio_pin_t pin)
 {
-    if(g_gpio.ops->read) return g_gpio.ops->read(pin);
+	if(!g_gpio.initialized) return;
+    if(g_gpio.ops && g_gpio.ops->read) return g_gpio.ops->read(pin);
 
     return GPIO_LOW;
 }
 
 void gpio_toggle(gpio_pin_t pin)
 {
-    if(g_gpio.ops->toggle) g_gpio.ops->toggle(pin);
+	if(!g_gpio.initialized) return;
+    if(g_gpio.ops && g_gpio.ops->toggle) g_gpio.ops->toggle(pin);
 }
 
 int gpio_irq_register(gpio_pin_t pin,
@@ -42,24 +46,27 @@ int gpio_irq_register(gpio_pin_t pin,
         gpio_irq_cb_t cb,
         void *ctx)
 {
-    if (g_gpio.ops->irq_register) return g_gpio.ops->irq_register(pin, edge, cb, ctx);
+	if(!g_gpio.initialized) return;
+    if(g_gpio.ops && g_gpio.ops->irq_register) return g_gpio.ops->irq_register(pin, edge, cb, ctx);
     return -1;
 }
 
 void gpio_irq_enable(gpio_pin_t pin)
 {
-    if (g_gpio.ops->irq_enable) g_gpio.ops->irq_enable(pin);
+	if(!g_gpio.initialized) return;
+    if(g_gpio.ops && g_gpio.ops->irq_enable) g_gpio.ops->irq_enable(pin);
 }
 
 void gpio_irq_disable(gpio_pin_t pin)
 {
-    if (g_gpio.ops->irq_disable) g_gpio.ops->irq_disable(pin);
+	if(!g_gpio.initialized) return;
+    if(g_gpio.ops && g_gpio.ops->irq_disable) g_gpio.ops->irq_disable(pin);
 }
 
 
 uint16_t gpio_get_pin(gpio_pin_t pin)
 {
-    return 1U << (pin & 0xFF);
+    return 1U << (pin & 0xFFFF);
 }
 
 uint16_t gpio_get_port(gpio_pin_t pin)
