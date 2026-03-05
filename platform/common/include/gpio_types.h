2a2b5613e9f1b70e03d46f0aaa6b3b8a47da4be1
Saurabh Daga <saurabh.daga@semaconnect.com>
2026-03-06 01:21:21 +0530

    make gpio_pull_t,gpio_mode_t,gpio_irq_edge_t,gpio_speed_t common for board & driver layer to avoid enum translation from layer to layer

---
+++ platform/common/include/gpio_types.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <stdint.h>
+
+typedef enum {
+    GPIO_MODE_INPUT,
+    GPIO_MODE_OUTPUT,
+    GPIO_MODE_OUTPUT_OD,
+    GPIO_MODE_AF,
+    GPIO_MODE_AF_OD,
+    GPIO_MODE_ANALOG
+} gpio_mode_t;
+
+typedef enum {
+    GPIO_PULL_NONE,
+    GPIO_PULL_UP,
+    GPIO_PULL_DOWN
+} gpio_pull_t;
+
+typedef enum {
+    GPIO_SPEED_LOW,
+    GPIO_SPEED_MEDIUM,
+    GPIO_SPEED_HIGH,
+    GPIO_SPEED_VERY_HIGH
+} gpio_speed_t;
+
+typedef enum {
+    GPIO_IRQ_EDGE_RISING,
+    GPIO_IRQ_EDGE_FALLING,
+    GPIO_IRQ_EDGE_BOTH
+} gpio_irq_edge_t;
